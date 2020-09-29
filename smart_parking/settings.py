fd5ccf705b41d345a7bd32e90f576dbf730d309b
Praveen Chaudhary <43620425+chaudharypraveen98@users.noreply.github.com>
2020-09-29 19:11:46 +0530

    Update settings.py

--- smart_parking/settings.py
+++ smart_parking/settings.py
@@ -125,5 +125,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'devilunknown1509@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bash@4767'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
