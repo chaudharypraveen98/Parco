89e89099fb2ada9ff77540304f7d76e3a29c2e2a
Praveen Chaudhary <chaudharypraveen98@gmail.com>
2021-07-26 23:31:39 +0530

    fix

--- booking/static/booking/js/firebase.js
+++ booking/static/booking/js/firebase.js
@@ -1,13 +1,13 @@
 //auto increment
 var firebaseConfig = {
-  apiKey: "AIzaSyD9v4wGnOfvPj5hFvT9RFHr51RHh3VhzxE",
-  authDomain: "smart-parking-51b36.firebaseapp.com",
-  databaseURL: "https://smart-parking-51b36.firebaseio.com",
-  projectId: "smart-parking-51b36",
-  storageBucket: "smart-parking-51b36.appspot.com",
-  messagingSenderId: "896172750089",
-  appId: "1:896172750089:web:107f3613b8b66ac74c0fd8",
-  measurementId: "G-ZCV4DBC045"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,54 +18,51 @@ $(document).ready(function(){
   let slot2;
   let slot3;
   let slot4;
+  let len1,len2,len3,len4;
 
   database.ref().on("value", function(snap){
     slot1 = snap.val().slot1;
     slot2 = snap.val().slot2;
     slot3 = snap.val().slot3;
     slot4 = snap.val().slot4;
-    console.log(slot1);
-
-    {if(slot1 ==="free"){
-      console.log("free");
-      insertSlot("slot1");
-    } else {
-      console.log("occupied");
-      removeSlot("slot1");
-    }}
-    {if(slot2 ==="free"){
-      console.log("free");
-      insertSlot("slot2");
-    } else {
-      console.log("occupied");
-      removeSlot("slot2");
-    }}
-    {if(slot3 ==="free"){
-      console.log("free");
-      insertSlot("slot3");
-    } else {
-      console.log("occupied");
-      removeSlot("slot3");
-    }}
-    {if(slot4 ==="free"){
-      console.log("free");
-      insertSlot("slot4");
-    } else {
-      console.log("occupied");
-      removeSlot("slot4");
-    }}
+    len1 = document.getElementsByClassName("slot1").length;
+    len2 = document.getElementsByClassName("slot2").length;
+    len3 = document.getElementsByClassName("slot3").length;
+    len4 = document.getElementsByClassName("slot4").length;
+    insertOption(len1, slot1, "slot1");
+    insertOption(len2, slot2, "slot2");
+    insertOption(len3, slot3, "slot3");
+    insertOption(len4, slot4, "slot4");
+    console.log("-----------");
   });
+  let insertOption=function (length,slotValue,slotTitle) {
+    console.log("value",length,slotTitle,slotValue);
+    if (length!=0){
+        if(slotValue !="free"){
+          console.log("removing free");
+          removeSlot(slotTitle);
+      } else {
+          console.log("already free")
+        }
+    } else{
+        if(slotValue ==="free"){
+          console.log("inserting new")
+          insertSlot(slotTitle);
+      } else {
+        console.log("already occupied")
+      }
+    }
+  }
   let insertSlot=function (param){
-    console.log(param)
-    $('#id-slot').append(
+    $('#id_slot_no').append(
       $('<option>').prop({
-        id: param,
-        innerHTML: "slot1 activated",
-        name: param,
+        value : param,
+        innerHTML: `${param}`,
+        className: param,
       })
     );
   };
   let removeSlot=function (param){
-    $(`#${param}`).remove()
+    $(`.${param}`).remove()
   };
 });
